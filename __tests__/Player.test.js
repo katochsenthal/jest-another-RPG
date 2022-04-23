54e05e53b1cf4e4e5b901b9a1c56c520d81a02c9
katochsenthal <katoch.senthal@gmail.com>
2022-04-23 14:55:11 -0400

    reduce health feature

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,6 +1,7 @@
 const Potion = require("../lib/Potion");
 jest.mock("../lib/Potion");
 const Player = require("../lib/Player");
+const { expect } = require("@jest/globals");
 
 console.log(new Potion());
 
@@ -34,4 +35,34 @@ test("get inventory from player or return false", () => {
   player.inventory = [];
 
   expect(player.getInventory()).toEqual(false);
+});
+
+test("gets player's health value", () => {
+  const player = new Player("Dave");
+
+  expect(player.getHealth()).toEqual(
+    expect.stringContaining(player.health.toString())
+  );
+});
+
+test("checks if player is alive or not", () => {
+  const player = new Player("Dave");
+
+  expect(player.isAlive()).toBeTruthy();
+
+  player.health = 0;
+
+  expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+  const player = new Player("Dave");
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+  expect(player.health).toBe(0);
 });
